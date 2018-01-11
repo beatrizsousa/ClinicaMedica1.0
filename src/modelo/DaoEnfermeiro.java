7ec6c221494bd7cdb5c842378e6625005f7f9081
tonifc007 <antonio007023@gmail.com>
2018-01-11 16:06:14 -0200

    Modificaçoes de Daoenfermeiro

--- src/modelo/DaoEnfermeiro.java
+++ src/modelo/DaoEnfermeiro.java
@@ -3,10 +3,7 @@ package modelo;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.ArrayList;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 
 public class DaoEnfermeiro {
